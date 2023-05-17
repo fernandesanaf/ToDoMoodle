ec215d56d782f556b86737ae2454371afb99ce9a
Ana Fernandes <ana.fernandes@scaladatacenters.com>
2023-05-17 10:16:59 -0300

    save

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import "./style/login.css"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from "./pages/login.jsx";
+
+import Login from "./pages/login";
+import Lista from "./pages/lista";
+import Calendario from "./pages/calendario";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Router>
+   <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/lista" element={<Lista />} />
+          <Route path="/calendario" element={<Calendario />} />
         </Routes>
-      </Router> */}
-      <div className="principal-login">
-        <div className="title-login1">
-          <div className="title-login2">
-            <h1>TO DO MOODLE</h1>
-          </div>
-          <div className="subtitle-login">
-            <h2>IFSP - Pirituba</h2>
-          </div>
-        </div>
-
-        <div className="body-login">
-          <button>Faça seu login pelo SUAP</button>
-          <div className="inputs-login">
-            <div className="insert-data-login">
-              <h2 style={{marginRight: 27}}>RA:</h2>
-              <input />
-            </div>
-
-            <div className="insert-data-login">
-              <h2 style={{marginRight: 5}}>Senha:</h2>
-              <input />
-            </div>
-          </div>
-
-        </div>
-        <div className="button-login"><button>ENTRAR</button> </div>
-
-      </div>
-    </div>
+      </BrowserRouter>
   );
 }
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React from "react";
+import "../style/login.css";
+import { Link } from "react-router-dom";
 
-function Login(){
 
-<div>
-      <h1>Página Inicial</h1>
+function Login() {
+
+  
+
+  return(
+    <div>
+    <div className="principal-login">
+      <div className="title-login1">
+        <div className="title-login2">
+          <h1>TO DO MOODLE</h1>
+        </div>
+        <div className="subtitle-login">
+          <h2>IFSP - Pirituba</h2>
+        </div>
+      </div>
+
+      <div className="body-login">
+        <button>Faça seu login pelo SUAP</button>
+        <div className="inputs-login">
+          <div className="insert-data-login">
+            <h2 style={{ marginRight: 31 }}>RA:</h2>
+            <input />
+          </div>
+
+          <div className="insert-data-login">
+            <h2 style={{ marginRight: 3 }}>Senha:</h2>
+            <input />
+          </div>
+        </div>
+      </div>
+      <div className="button-login">
+       <button onClick={(e) => {e.preventDefault(); window.location.href="http://localhost:3000/calendario"}}><h2>ENTRAR</h2></button>
+      </div>
     </div>
+  </div>
+  )
 }
 export default Login;
